d8a0ce61af18e35c9ce9d0b642be653d1df83014
Rishi Keshri <rishikeshri424@gmail.com>
2025-09-03 01:40:35 +0530

    Update User.java

--- src/main/java/com/example/CityGenie/entity/User.java
+++ src/main/java/com/example/CityGenie/entity/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -12,6 +15,8 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
